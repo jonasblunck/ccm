cf9c6103deee9202ddbfef06197afa184d75ed88
jonasbl <jonas.blunck@blunck.se>
2014-08-17 17:59:01 +0200

    Adding shelve for VS package

---
+++ ccmPack/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace JonasBlunck.ccmPack
+{
+    static class PkgCmdIDList
+    {
+
+        public const uint cmdIdToolWindow =    0x101;
+
+    };
+}
