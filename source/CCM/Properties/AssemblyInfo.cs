a9f53ca4e90bd2c1c6e7183aa66fb41f54276254
Jonas Blunck <jonasbl@microsoft.com>
2018-06-02 14:30:31 +0200

    Version info update

--- source/CCM/Properties/AssemblyInfo.cs
+++ source/CCM/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.11")]
-[assembly: AssemblyFileVersion("1.1.11")]
+[assembly: AssemblyVersion("1.1.12")]
+[assembly: AssemblyFileVersion("1.1.12")]
