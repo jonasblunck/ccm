87942f3c86e0b3a4b10194b146774a5f5d3c7c05
Jonas Blunck <jonasbl@microsoft.com>
2022-12-14 15:28:49 +0100

    Make code build

--- source/CCMTests/DriverTests.cs
+++ source/CCMTests/DriverTests.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CCM;
 using CCMEngine;
 using System.IO;
 using System.Xml;
--- source/CCMTests/ProgramTests.cs
+++ source/CCMTests/ProgramTests.cs
@@ -1,6 +1,5 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CCM;
 using System.Xml;
 using CCMEngine;
 
